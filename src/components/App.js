cec39062289ffbe3c8a9cf75249df65239cadabb
NatalieGichuki <nataliegichuki@gmail.com>
2025-04-29 10:59:47 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+
 function App() {
+
   return (
     <div className="app">
       <Header />
       <PlantPage />
+      
     </div>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
